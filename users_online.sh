ea0378407b0c608e1672d94966b90bda1678f7b9
1Ruhisingh1 <85754019+1Ruhisingh1@users.noreply.github.com>
2021-06-12 20:39:52 +0530

    Update users_online.sh
    Changed the sleep command to 3 for beet user expericence.

--- users_online.sh
+++ users_online.sh
@@ -14,7 +14,7 @@ echo "Hello, $usr_name and welcome"
 #cheking and printing all current users logged in 
 echo "checking for all logged in users "
 echo "............. "
-sleep 4
+sleep 3
 
 echo \
 
